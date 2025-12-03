9d41010c657a9356199e314fd29e733c57a42725
David Vos <vosdavid2@gmail.com>
2025-12-03 11:30:02 +0100

    Day 03 part 2

--- src/day/day03.rs
+++ src/day/day03.rs
@@ -40,21 +40,30 @@ impl Day for Day03 {
             .map(|bank| {
                 let batteries = bank
                     .chars()
-                    .map(|c| c.to_digit(10).unwrap())
-                    .collect::<Vec<u32>>();
-                let mut max = 0;
-                for i in 0..batteries.len() {
-                    for j in i + 1..batteries.len() {
-                        let candidate = batteries[i] * 10 + batteries[j];
-                        if candidate > max {
-                            max = candidate;
-                        }
-                    }
-                }
-                max
+                    .map(|c| c.to_digit(10).unwrap() as u64)
+                    .collect::<Vec<u64>>();
+                find_joltage(&batteries, 0, 12)
             })
-            .sum::<u32>();
+            .sum::<u64>();
 
         result.to_string()
+    }
+}
+
+fn find_joltage(batteries: &[u64], current: u64, digits: usize) -> u64 {
+    let mut max = 0;
+    let mut max_i = 0;
+    let candidates = &batteries[0..(batteries.len() - digits + 1)];
+    for (i, battery) in candidates.iter().enumerate() {
+        let candidate = current * 10 + battery;
+        if candidate > max {
+            max = candidate;
+            max_i = i;
+        }
+    }
+    if batteries.len() > 1 && digits > 1 {
+        find_joltage(&batteries[max_i + 1..], max, digits - 1)
+    } else {
+        max
     }
 }
